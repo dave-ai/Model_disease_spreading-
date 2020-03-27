876bf04a5cb0c841c4ad13e42cf09fa686f2a527
DaveB <davide1.grande@mail.polimi.it>
2020-03-27 02:27:17 +0000

    Added deseaseCourseV2.m: function modified accordingly to the definition of the methods of PersonClassV3.

---
+++ deseaseCourseV2.m
@@ -0,0 +1,31 @@
+function [output] = deseaseCourseV2(person)
+%DESEASECOURSE establish what is the course of the desease of each patient;
+% it is based on the statistics data for Covid-19 available at March the
+% 19th 2020.
+%
+% The infected person might get a standard course of the desease with mild
+% sympthoms, might required to get hospitalized or might die
+%
+%   
+%   original author: Davide Grande
+%   date: 19-March-2020
+%
+
+
+    outcome = randi(100,1);
+    if outcome <= 80
+        person.health_status = 5;
+    elseif outcome >= 96
+        person.health_status = 8;
+    else
+        person.health_status = 6;
+    end
+
+    % person.sick_in_isolation = false;
+
+    output = person;
+end
+
+
+
+
