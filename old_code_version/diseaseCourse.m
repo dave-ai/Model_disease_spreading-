809ded1b5019810864b674f1fa2be4938b0e2fb8
dave-ai <davidegrande9@gmail.com>
2020-05-17 22:49:44 +0100

    [Issue #5] Order old code versions
    Moving the old-logic code in the old_code_version folder.

---
+++ old_code_version/diseaseCourse.m
@@ -0,0 +1,28 @@
+function [output] = diseaseCourse(person)
+%DISEASECOURSE establish what is the course of the disease of each patient;
+% it is based on the statistics data for Covid-19 available at March the
+% 19th 2020.
+%
+% The infected person might get a standard course of the disease with mild
+% sympthoms, might required to get hospitalized or might die
+%
+%   
+%   original author: Davide Grande
+%   date: 19-March-2020
+%
+
+
+outcome = randi(100,1);
+if outcome <= 80
+    person.healing = true;
+elseif outcome >= 96
+    person.fatal_sickness = true;
+else
+    person.hospitalized = true;
+end
+
+% person.sick_in_isolation = false;
+
+output = person;
+end
+
